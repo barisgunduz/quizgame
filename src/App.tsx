3cbe76c67d8da4ed295003b1871958d810ef1c6b
Barış Gündüz <baris.gunduz@devstup.org>
2024-01-18 20:39:56 +0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,23 @@
+import Button from "react-bootstrap/Button";
+
+function App() {
+	return (
+		<>
+			<div className="quizQuestion">
+				<h3>
+					Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+					Ducimus natus unde quia repudiandae accusamus voluptatum
+					nesciunt excepturi consequuntur laudantium sed cumque,
+					aperiam dolore at fugiat vitae amet corporis aliquid
+					explicabo.
+				</h3>
+				<Button variant="primary">Answer 1</Button>
+				<Button variant="secondary">Answer 2</Button>
+				<Button variant="success">Answer 3</Button>
+				<Button variant="warning">Answer 4</Button>
+			</div>
+		</>
+	);
+}
+
+export default App;
